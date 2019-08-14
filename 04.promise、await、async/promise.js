fdb570c2b6d9a8b3560ffedef94d1adfe742be1e
yangchaolin <289459620@qq.com>
2019-08-14 20:30:42 +0800

    成功后能调用reslove传值

--- 04.promise、await、async/promise.js
+++ 04.promise、await、async/promise.js
@@ -2,7 +2,37 @@
  * @Promise
  * @author yangchaolin
  */
-function Promise(){
-	return{}
+
+
+
+//传入一个函数，函数有两个形参---->reslove,reject
+function Promise(processor){
+	this.status='pending';
+	
+	//执行一下processor函数主体部分,并且定义函数的两个参数
+	processor(function reslove(res){
+		//成功后执行的函数
+		this.status='fulFilled';
+		console.log('调用了reslove')
+		console.log('收到值',res);
+		
+	},function reject(err){
+		console.log('调用了reslove')
+		//失败后执行的函数
+		this.status='reject';
+		
+	})
+
 }
+
+Promise.prototype={
+	constructor:Promise,
+	
+}
+
+var res111=new Promise((reslove,reject)=>{
+	console.log('进入第一步');
+	reslove(1000);
+})
+
 module.exports=Promise;
