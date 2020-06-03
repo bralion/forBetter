812fd6ad83938fad1d8f36c1aeb99b10182d9d7a
yangchaolin <289459620@qq.com>
2020-06-03 21:37:46 +0800

    今日头条版本webpack + es6 import函数导入模块

--- 今日头条/toutiao4-es6-webpack-importFunction/src/manager.js
+++ 今日头条/toutiao4-es6-webpack-importFunction/src/manager.js
@@ -3,40 +3,42 @@
 * @author Yangchaolin
 * @date 2020/6/3
 */
-import('./components.js').then(components=>{
-	const manager = {
-		name:'manager',
-		listen:function(){
-			window.onscroll = function (e) {
-				var distance = document.documentElement.offsetHeight - window.screen.height - window.scrollY;
-				if (distance < 100) {
-					main ();
-				}
-			};
-		},
-		getData:function () {
-			return fetch ('http://localhost:8099/list').then (res => {
-				return res.json ();
-			}).then (res => {
-				return res.data
-			});
-		},
-		renderData:function (data = []) {
-			var container = document.getElementById ('container')
-			var flagFrame = container.innerHTML;
-			let renderMap = components;
+
+const manager = {
+	name:'manager',
+	listen:function(){
+		window.onscroll = function (e) {
+			var distance = document.documentElement.offsetHeight - window.screen.height - window.scrollY;
+			if (distance < 100) {
+				main ();
+			}
+		};
+	},
+	getData:function () {
+		return fetch ('http://localhost:8099/list').then (res => {
+			return res.json ();
+		}).then (res => {
+			return res.data
+		});
+	},
+	renderData:function (data = []) {
+		var container = document.getElementById ('container')
+		var flagFrame = container.innerHTML;
+		import('./components.js').then(({default:components})=>{
+			console.log(components);
 			data.map (item => {
 				console.log(item)
-				flagFrame += renderMap[item.type] (item)
+				flagFrame += components[item.type] (item)
 			})
 			container.innerHTML = flagFrame;
-		},
-		init : function () {
-			this.getData ().then (data => {
-				this.renderData (data)
-			});
-			this.listen ();
-		}
+		})
+	},
+	init : function () {
+		this.getData ().then (data => {
+			this.renderData (data)
+		});
+		this.listen ();
 	}
-})
+}
+
 export default manager
