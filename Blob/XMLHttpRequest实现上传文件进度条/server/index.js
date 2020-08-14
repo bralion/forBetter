e84d41e68beb4b3e9cb6bd27308ece5f0e81f861
yangchaolin <289459620@qq.com>
2020-08-14 18:19:11 +0800

    服务器原理

--- Blob/XMLHttpRequest实现上传文件进度条/server/index.js
+++ Blob/XMLHttpRequest实现上传文件进度条/server/index.js
@@ -3,23 +3,32 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 	// req 是一个 http.IncomingMessage 实例，它是可读流。
 	// res 是一个 http.ServerResponse 实例，它是可写流。
-	
+	res.setHeader("Access-Control-Allow-Origin","*");
 	let body = '';
 	// 接收数据为 utf8 字符串，
 	// 如果没有设置字符编码，则会接收到 Buffer 对象。
 	req.setEncoding('utf8');
 	
 	// 如果添加了监听器，则可读流会触发 'data' 事件。
+	var count=0;
 	req.on('data', (chunk) => {
+		console.log('shoudao',chunk.toString('utf8'));
 		body += chunk;
+		count++
+		if(count===30){
+			debugger
+		}
 	});
 	
 	// 'end' 事件表明整个请求体已被接收。
 	req.on('end', () => {
+		console.log(count,'count');
 		try {
-			const data = JSON.parse(body);
+			console.log('end')
+			const data = body;
+			console.log(data,'data')
 			// 响应信息给用户。
-			res.write(typeof data);
+			res.write(typeof body);
 			res.end();
 		} catch (er) {
 			// json 解析失败。
@@ -29,7 +38,7 @@ const server = http.createServer((req, res) => {
 	});
 });
 
-server.listen(1337);
+server.listen(1338);
 
 // $ curl localhost:1337 -d "{}"
 // object
---
+++ Blob/理解Buffer.js
@@ -0,0 +1,9 @@
+
+var bf1=new Buffer('miao','utf-8');
+console.log(bf1);
+for(var i=0;i<bf1.length;i++){
+    console.log(String.fromCharCode(bf1[i]));
+}
+
+//https://www.cnblogs.com/xuxiaozhi/p/7844673.html
+
