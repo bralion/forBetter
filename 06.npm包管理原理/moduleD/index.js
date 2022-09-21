7fb8159a5102a198383344cc632f4851c2df1c04
yangcl120 <289459620@qq.com>
2022-09-21 19:55:52 +0800

    npm包管理探究

---
+++ 06.npm包管理原理/moduleD/index.js
@@ -0,0 +1,6 @@
+import a from 'module_a'
+import b from 'module_b'
+export default {
+    name:'module_d',
+    a,b
+}
---
+++ 06.npm包管理原理/useInProject/2.嵌套引用/index.js
@@ -0,0 +1,5 @@
+import a from 'module_a'
+import d from 'module_d'
+export default {
+    a,d
+}
